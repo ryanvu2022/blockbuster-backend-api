99f1cab8d6e8f39ea2a4afe99949081e963f84df
Thinh <ryanvu2022@gmail.com>
2022-12-19 01:11:31 -0500

    complete signin functionality

---
+++ middleware/auth.js
@@ -0,0 +1,12 @@
+const auth = (req, res, next) => {
+   // console.log(req.headers.data);
+   try {
+       req.userId = req.headers._id ? req.headers._id : req.headers.sub;
+      //  console.log(req.userId);
+       next();
+   } catch (error) {
+       console.log(error);
+   }
+}
+
+export default auth;
